d97de71127d2cb444714bad250856e7392ed6c3f
Logan Bleyl <loganmbleyl05@gmail.com>
2026-02-06 15:37:23 -0500

    block to html

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+from htmlnode import ParentNode, LeafNode, text_node_to_html_node, HTMLNode
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType
 
 
 def markdown_to_blocks(markdown):
@@ -64,4 +67,98 @@ def block_to_block_type(markdown):
             return BlockType.ORDERED_LIST
 
     return BlockType.PARAGRAPH
-    
+
+def markdown_to_html_node(markdown):
+    md_block = markdown_to_blocks(markdown)
+    nodes = []
+
+    for block in md_block:
+        block_type = block_to_block_type(block)
+
+        if block_type == BlockType.PARAGRAPH:
+            lines = block.split("\n")
+            paragraph = " ".join(lines)
+            html_node = ParentNode("p", children=text_to_children(paragraph))
+            nodes.append(html_node)
+
+        elif block_type == BlockType.HEADING:
+            level = heading_level(block)
+            tag = f"h{level}"
+            clean = block[level + 1:]
+
+            html_node = ParentNode(tag, children=text_to_children(clean))
+            nodes.append(html_node)
+
+        elif block_type == BlockType.CODE:
+            lines = block.split("\n")
+            inner_lines = lines[1:-1]
+            clean = "\n".join(inner_lines) + "\n"
+            
+            text_node = TextNode(clean, TextType.TEXT)
+            html_node = text_node_to_html_node(text_node)
+            code_node = ParentNode("code", children=[html_node])
+            pre_node = ParentNode("pre", children=[code_node])
+            nodes.append(pre_node)
+
+        elif block_type == BlockType.QUOTE:
+            new_lines = []
+            lines = block.split("\n")
+            for line in lines:
+                new_line = line[1:].lstrip()
+                new_lines.append(new_line)
+            
+            clean = " ".join(new_lines)
+            html_node = ParentNode("blockquote", children=text_to_children(clean))
+            nodes.append(html_node)
+
+        elif block_type == BlockType.UNORDERED_LIST:
+            new_lines = []
+            lines = block.split("\n")
+            for line in lines:
+                inner_lines = line[2:]
+                new_line = ParentNode("li", children=text_to_children(inner_lines))
+                new_lines.append(new_line)
+            html_node = ParentNode("ul", children=new_lines)
+            nodes.append(html_node)
+
+        elif block_type == BlockType.ORDERED_LIST:
+            new_lines = []
+            lines = block.split("\n")
+            for line in lines:
+                parts = line.split(". ", 1)
+                item_text = parts[1]
+                new_line = ParentNode("li", children=text_to_children(item_text))
+                new_lines.append(new_line)
+            html_node = ParentNode("ol", children=new_lines)
+            nodes.append(html_node)
+
+    return ParentNode(tag="div", children=nodes)
+
+
+def text_to_children(text):
+    html_nodes = []
+    text_nodes = text_to_textnodes(text)
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        html_nodes.append(html_node)
+    return html_nodes
+
+def heading_level(markdown):
+    if markdown.startswith("###### "):
+        return 6
+        
+    elif markdown.startswith("##### "):
+        return 5
+
+    elif markdown.startswith("#### "):
+        return 4
+
+    elif markdown.startswith("### "):
+        return 3
+
+    elif markdown.startswith("## "):
+        return 2
+
+    elif markdown.startswith("# "):
+        return 1
+
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,7 @@
 import unittest
-from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type, markdown_to_html_node, text_to_children, heading_level
+from htmlnode import HTMLNode, ParentNode, LeafNode, text_node_to_html_node
+from textnode import TextNode, TextType
 
 class TestMarkdownToBlocks(unittest.TestCase):
     
@@ -45,25 +47,38 @@ This is the same paragraph on a new line
             ],
         )
 
-        # Blocktype Tests 
+    # Blocktype Tests 
     
-    def test_block_to_blocktype(self):
-        md = "> This is a quote.\n> It can span multiple lines.\n> Every line starts with a greater-than symbol."
-        block = block_to_block_type(md)
-        block_type = BlockType.QUOTE
-        self.assertEqual(block, block_type)
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
 
-    def test_block_to_blocktype_unmatched_quote(self):
-        md = "> This is a quote.\n It can span multiple lines.\n Every line starts with a greater-than symbol."
-        block = block_to_block_type(md)
-        block_type = BlockType.QUOTE
-        self.assertNotEqual(block, block_type)
+This is another paragraph with _italic_ text and `code` here
 
-    def test_block_to_blocktype_ordered_list(self):
-        md = "1. first line.\n2. second line.\n3. third line"
-        block = block_to_block_type(md)
-        block_type = BlockType.ORDERED_LIST
-        self.assertEqual(block, block_type)
+"""
 
-        if __name__ == "__main__":
-            unittest.main()
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+    )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+        html,
+        "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    )
+    if __name__ == "__main__":
+        unittest.main()
