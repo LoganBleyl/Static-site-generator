748d1d0b1cb0a6ed28b5a748135285c6e0fda84c
Logan Bleyl <loganmbleyl05@gmail.com>
2026-02-07 20:41:07 -0500

    static copy

--- src/main.py
+++ src/main.py
@@ -1,8 +1,33 @@
 from enum import Enum
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_recursive(src, dst):
+    for name in os.listdir(src):
+        item_src = os.path.join(src, name)
+        item_dst = os.path.join(dst, name)
+    
+        if os.path.isfile(item_src):
+            shutil.copy(item_src, item_dst)
+        else:
+            os.mkdir(item_dst)
+            copy_recursive(item_src, item_dst)
 
 def main():
     node =TextNode("some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
-main()
+    
+    this_dir = os.path.dirname(__file__)
+    project_root = os.path.join(this_dir, "..")
+    public_dir = os.path.join(project_root, "public")
+    static_dir = os.path.join(project_root, "static")
 
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+
+    os.mkdir(public_dir)
+
+    copy_recursive(static_dir, public_dir)
+
+main()
